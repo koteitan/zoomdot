c91e3b82aa5de50631d093671e51808a1e8652af
koteitan <ukaibutton@mac.com>
2016-05-05 22:41:06 +0900

    first add

---
+++ version.js
@@ -0,0 +1,9 @@
+document.getElementById("versiondisplay").innerHTML = "0.1";
+document.getElementById("changelog").innerHTML = 
+"<ul>"+
+"<li>2016/05/05 22:32 実装開始。</li>"+
+"</ul>";
+document.getElementById("knownbugs").innerHTML = 
+"<ul>"+
+"<li>まだ実験中。</li>"+
+"</ul>";
