fad93e5188be56a0b42ee2808f3fa0aa6dbcbff4
koteitan <ukaibutton@mac.com>
2016-05-07 19:31:52 +0900

    IIR smoothing is implemented.

--- main.js
+++ main.js
@@ -17,23 +17,23 @@ window.onload=function(){ //entry point
 
 
 function loadImg(e){
-	var file = e.target.files[0];
-	FRIn = new FileReader();
-	FRIn.onload = function(e){
-		imgIn = new Image();
-		imgIn.onload = function(){
-		  wx=imgIn.width;
-		  wy=imgIn.height;
-		  elemCanvasIn .width  = wx; elemCanvasIn .height = wy;
-		  elemCanvasOut.width  = wx; elemCanvasOut.height = wy;
-		  imgOut       .width  = wx; imgOut       .height = wy;
-			elemCanvasIn .getContext('2d').drawImage(imgIn, 0, 0, wx, wy);
-			analize();
-			outImg();
-		}
-		imgIn.src = e.target.result;
-	};
-	FRIn.readAsDataURL(file);
+  var file = e.target.files[0];
+  FRIn = new FileReader();
+  FRIn.onload = function(e){
+    imgIn = new Image();
+    imgIn.onload = function(){
+      wx=imgIn.width;
+      wy=imgIn.height;
+      elemCanvasIn .width  = wx; elemCanvasIn .height = wy;
+      elemCanvasOut.width  = wx; elemCanvasOut.height = wy;
+      imgOut       .width  = wx; imgOut       .height = wy;
+      elemCanvasIn .getContext('2d').drawImage(imgIn, 0, 0, wx, wy);
+      analize();
+      outImg();
+    }
+    imgIn.src = e.target.result;
+  };
+  FRIn.readAsDataURL(file);
 }
 
 var trendR  = 40;
@@ -42,17 +42,17 @@ var trendSD = 40;
 var invTrendSD = 1/trendSD;
 function analize(){
   // ctx -> make gray -> A
-	var idIn = elemCanvasIn.getContext('2d').getImageData(0, 0, wx, wy);
-	var cpaIn = idIn.data;
-	var A = new Array(wy);
-	for (y = 0; y < wy ; y++){
-  	A[y] = new Array(wx);
-		for (x = 0; x < wx; x++){
-			var i = (x + y * wx) * 4/* RGBA */;
-			A[y][x] = (cpaIn[i]+cpaIn[i+1]+cpaIn[i+2])/3*255;
-		}
-	}
-	//remove trend
+  var idIn = elemCanvasIn.getContext('2d').getImageData(0, 0, wx, wy);
+  var cpaIn = idIn.data;
+  var A = new Array(wy);
+  for (y = 0; y < wy ; y++){
+    A[y] = new Array(wx);
+    for (x = 0; x < wx; x++){
+      var i = (x + y * wx) * 4/* RGBA */;
+      A[y][x] = (cpaIn[i]+cpaIn[i+1]+cpaIn[i+2])/3*255;
+    }
+  }
+  //remove trend
   if(trendR>0){
     var tA = new Array(wy);
     for (y = 0; y < wy ; y++) tA[y] = new Array(wx);
@@ -91,14 +91,6 @@ function analize(){
             tA[y2][x2] = medsubA;
           }
         }
-/*        elemDebug.innerHTML = elemDebug.innerHTML+ 
-          "x="+x+", y="+y+"<br>"+
-          "rx0="+rx0+", rx1="+rx1+"<br>"+
-          "ry0="+ry0+", ry1="+ry1+"<br>"+
-          "dx0="+dx0+", dx1="+dx1+"<br>"+
-          "dy0="+dy0+", dy1="+dy1+"<br>"+
-          "rankR="+rankR.toString()+"<br>"+
-          "medsubA="+medsubA+"<br><br>";*/
       }//x
     }//y
     for(var y=0;y<wy;y++){
@@ -106,48 +98,87 @@ function analize(){
         A[y][x]=A[y][x]-tA[y][x];
       }
     }
-	}
-	A=normalize(A);
-	
+  }
+  
+  //IIR smoothing
+  decay = 0.75;
+  //x
+  for(var y=0;y<wy;y++){
+    var h;
+    //forward
+    h=A[y][0];
+    for(var x=1;x<wx;x++){
+      h = h*decay+(1-decay)*A[y][x];
+      A[y][x]=h;
+    }
+    //backword
+    h=A[y][wx-1];
+    for(var x=wx-1;x>=0;x--){
+      h = h*decay+(1-decay)*A[y][x];
+      A[y][x]=h;
+    }
+  }
+  //y
+  for(var x=0;x<wx;x++){
+    var h;
+    //forward
+    h=A[0][x];
+    for(var y=1;y<wy;y++){
+      h = h*decay+(1-decay)*A[y][x];
+      A[y][x]=h;
+    }
+    //backword
+    h=A[wy-1][x];
+    for(var y=wy-1;y>=0;y--){
+      h = h*decay+(1-decay)*A[y][x];
+      A[y][x]=h;
+    }
+  }
+  
+
+  
+  A=normalize(A);
+  
+  
   // A -> ctx
-	var idOut = elemCanvasOut.getContext('2d').createImageData(wx,wy);
-	var cpaOut = idOut.data;
-	for (y = 0; y < wy ; y++){
-		for (x = 0; x < wx; x++){
-			var i = (x + y * wx) * 4/* RGBA */;
-			var a = Math.floor(A[y][x]*255);
-			cpaOut[i+0] = a;
-			cpaOut[i+1] = a;
-			cpaOut[i+2] = a;
-			cpaOut[i+3] = 255;/*A=255*/
-		}
-	}
-	elemCanvasOut.getContext('2d').putImageData(idOut, 0, 0);
+  var idOut = elemCanvasOut.getContext('2d').createImageData(wx,wy);
+  var cpaOut = idOut.data;
+  for (y = 0; y < wy ; y++){
+    for (x = 0; x < wx; x++){
+      var i = (x + y * wx) * 4/* RGBA */;
+      var a = Math.floor(A[y][x]*255);
+      cpaOut[i+0] = a;
+      cpaOut[i+1] = a;
+      cpaOut[i+2] = a;
+      cpaOut[i+3] = 255;/*A=255*/
+    }
+  }
+  elemCanvasOut.getContext('2d').putImageData(idOut, 0, 0);
 }
 function outImg(){
-	var src = elemCanvasOut.toDataURL('image/jpg');
-	elemImgOut.src = src;
+  var src = elemCanvasOut.toDataURL('image/jpg');
+  elemImgOut.src = src;
 }
 
 var normalize = function(A){
-	var wy = A.length;
-	var wx = A[0].length;
-	var maxA = A[0][0];
-	var minA = A[0][0];
-	for (y = 0; y < wy ; y++){
-	  for (x = 0; x < wx; x++){
-	    var a=A[y][x];
-	    if     (a>maxA) maxA=a;
-	    else if(a<minA) minA=a;
-	  }
-	}
-	var g=1/(maxA-minA);
-	var B=new Array(wy);
-	for (y = 0; y < wy ; y++){
-	  B[y]=new Array(wx);
-	  for (x = 0; x < wx; x++){
-	    B[y][x]=(A[y][x]-minA)*g;
-	  }
-	}
-	return B;
+  var wy = A.length;
+  var wx = A[0].length;
+  var maxA = A[0][0];
+  var minA = A[0][0];
+  for (y = 0; y < wy ; y++){
+    for (x = 0; x < wx; x++){
+      var a=A[y][x];
+      if     (a>maxA) maxA=a;
+      else if(a<minA) minA=a;
+    }
+  }
+  var g=1/(maxA-minA);
+  var B=new Array(wy);
+  for (y = 0; y < wy ; y++){
+    B[y]=new Array(wx);
+    for (x = 0; x < wx; x++){
+      B[y][x]=(A[y][x]-minA)*g;
+    }
+  }
+  return B;
 }
