d2e1979bc01072b561745c20077579215731e542
koteitan <ukaibutton@mac.com>
2020-06-19 20:04:51 +0900

    bugfix: wrong size in saved image change size after loading img

--- main.js
+++ main.js
@@ -1,95 +1,109 @@
-var FRIn ;
-var imgIn;
-var elemCanvasIn ;
-var elemCanvasOut;
-var elemImgOut   ;
+var frin ;
+var imgin;
+var elemcanvasin ;
+var elemcanvasout;
+var elemimgout   ;
 
 var ratio;
 var colors = 4;
-var wx = 320;
-var wy = 240;
-var wx2;
-var wy2;
-
+var wxin = 320;
+var wyin = 240;
+var wxout;
+var wyout;
+var isimgloaded;
 
 /*----------------------------
-  window.onload(){
-    loadImg(e){
+ user ->
+  window.onload()   -> redraw()
+  fileselect-change -> handleloadimg(e) -> redraw()
+  ratio-onchange    -> redraw()
+
+ redraw(e){
+    changesize();
+    if(imgloaded){
       analize();
-      outImg();
     }
   }
- * */
+ ---------------------------*/
+
+/*user interfaces ----------------*/
 window.onload=function(){ //entry point
-  elemCanvasIn  = document.getElementById('canvasIn');
-  elemCanvasOut = document.getElementById('canvasOut');
-  elemImgOut    = document.getElementById('imgOut');
+  elemcanvasin  = document.getElementById('canvavsin');
+  elemcanvasout = document.getElementById('canvasout');
+  elemimgout    = document.getElementById('imgout');
   elemDebug     = document.getElementById('debug');
-  document.getElementById('fileSelect').addEventListener('change', loadImg, false);
-  changeratio();
+  document.getElementById('fileselect').addEventListener('change', handleloadimg, false);
+
+  wxin=320; wyin=240;
+  isimgloaded = false;
+  handlechangeratio();
+  redraw();
 }
-
-
-function loadImg(e){
+var handlechangeratio = function(){
+  ratio = form0.ratio.value;
+  redraw();
+};
+function handleloadimg(e){
   var file = e.target.files[0];
-  FRIn = new FileReader();
-  FRIn.onload = function(e){
-    imgIn = new Image();
-    imgIn.onload = function(){
-      wx=imgIn.width;
-      wy=imgIn.height;
-      elemCanvasIn .width  = wx;  elemCanvasIn .height = wy;
-      wx2=wx*ratio;
-      wy2=wy*ratio;
-      elemCanvasOut.width  = wx2; elemCanvasOut.height = wy2;
-      imgOut       .width  = wx2; imgOut       .height = wy2;
-      elemCanvasIn .getContext('2d').drawImage(imgIn, 0, 0, wx, wy);
-      analize();
-      outImg();
+  frin = new FileReader();
+  frin.onload = function(e){
+    imgin = new Image();
+    imgin.onload = function(){
+      wxin=imgin.width;
+      wyin=imgin.height;
+      isimgloaded = true;
+      redraw();
     }
-    imgIn.src = e.target.result;
+    imgin.src = e.target.result;
   };
-  FRIn.readAsDataURL(file);
+  frin.readAsDataURL(file);
 }
 
-function analize(){
+/* internal subroutines -------------------*/
+var redraw = function(){
+  changesize();
+  if(isimgloaded){
+    analize();
+  }
+};
+var changesize = function(){
+  wxout                  = wxin*ratio;
+  wyout                  = wyin*ratio;
+  elemcanvasin.width   = wxin;
+  elemcanvasin.height  = wyin;
+  elemcanvasout.width  = wxout;
+  elemcanvasout.height = wyout;
+  imgout       .width  = wxout;
+  imgout       .height = wyout;
+}
+var analize = function(){
   // ctx -> A
-  var idIn = elemCanvasIn.getContext('2d').getImageData(0, 0, wx, wy);
-  var cpaIn = idIn.data;
-  var A = new Array(wy);
-  for (y = 0; y < wy ; y++){
-    A[y] = new Array(wx);
-    for (x = 0; x < wx; x++){
+  elemcanvasin.getContext('2d').drawImage(imgin, 0, 0, wxin, wyin);
+  var idin = elemcanvasin.getContext('2d').getImageData(0, 0, wxin, wyin);
+  var cpain = idin.data;
+  var A = new Array(wyin);
+  for (y = 0; y < wyin ; y++){
+    A[y] = new Array(wyin);
+    for (x = 0; x < wxin; x++){
       A[y][x] = new Array(colors);
       for (c = 0; c < colors; c++){
-        A[y][x][c] = cpaIn[(y*wx+x)*colors+c];
+        A[y][x][c] = cpain[(y*wxin+x)*colors+c];
       }
     }
   }
-  wx2=wx*ratio;
-  wy2=wy*ratio;
   // A -> ctx
-  var idOut = elemCanvasOut.getContext('2d').createImageData(wx2,wy2);
-  var cpaOut = idOut.data;
-  for (y = 0; y < wy2 ; y++){
-    for (x = 0; x < wx2; x++){
+  var idout = elemcanvasout.getContext('2d').createImageData(wxout,wyout);
+  var cpaout = idout.data;
+  for (y = 0; y < wyout ; y++){
+    for (x = 0; x < wxout; x++){
       for (c = 0; c < colors; c++){
-        cpaOut[(y*wx2+x)*colors+c] = A[Math.floor(y/ratio)][Math.floor(x/ratio)][c];
+        cpaout[(y*wxout+x)*colors+c] = A[Math.floor(y/ratio)][Math.floor(x/ratio)][c];
       }
     }
   }
-  var ctxOut = elemCanvasOut.getContext('2d');
-  ctxOut.putImageData(idOut, 0, 0);
-}
-function outImg(){
-  var src = elemCanvasOut.toDataURL('image/jpg');
-  elemImgOut.src = src;
+  var ctxout = elemcanvasout.getContext('2d');
+  ctxout.putImageData(idout, 0, 0);
+  var src = elemcanvasout.toDataURL('image/jpg');
+  elemimgout.src = src;
 }
 
-var changeratio = function(){
-  ratio = form0.ratio.value;
-  wx2 = wx*ratio;
-  wy2 = wy*ratio;
-  elemCanvasOut.width  = wx2; elemCanvasOut.height = wy2;
-  imgOut       .width  = wx2; imgOut       .height = wy2;
-};
