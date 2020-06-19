642ccfa599532a59a7194dd6b168a510e7ff5ef9
koteitan <ukaibutton@mac.com>
2020-06-19 22:46:14 +0900

    change design

--- version.js
+++ version.js
@@ -5,7 +5,7 @@ document.getElementById("changelog").innerHTML =
 "<li>2016/06/19 01:45 Start."+
 "<li>2016/06/19 02:41 Implemented."+
 "<li>2016/06/19 20:00 The bug in which change size after load img occurs wrong size for saving was fixed."+
-"<li>2016/06/19 22:00 Enlarged UI."+
+"<li>2016/06/19 22:45 Enlarged UI."+
 "</ul>";
 document.getElementById("knownbugs").innerHTML = 
 "<h3>KnownBug</h3>"+
