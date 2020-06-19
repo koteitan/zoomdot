f7fce01bff6337f24523e92a52f78194e4bf5e67
koteitan <ukaibutton@mac.com>
2020-06-19 20:04:51 +0900

    bugfix: wrong size in saved image change size after loading img

--- version.js
+++ version.js
@@ -1,9 +1,10 @@
-document.getElementById("versiondisplay").innerHTML = "1.0";
+document.getElementById("versiondisplay").innerHTML = "1.1";
 document.getElementById("changelog").innerHTML = 
 "<h3>ChangeLog</h3>"+
 "<ul>"+
 "<li>2016/06/19 01:45 Start."+
 "<li>2016/06/19 02:41 Implemented."+
+"<li>2016/06/19 20:00 The bug in which change size after load img occurs wrong size for saving was fixed."+
 "</ul>";
 document.getElementById("knownbugs").innerHTML = 
 "<h3>KnownBug</h3>"+
