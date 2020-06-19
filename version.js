616d0d7186a1b880cc8108de8c287c1a7f5b4e83
koteitan <ukaibutton@mac.com>
2020-06-19 22:21:05 +0900

    enlarged UI.

--- version.js
+++ version.js
@@ -1,10 +1,11 @@
-document.getElementById("versiondisplay").innerHTML = "1.1";
+document.getElementById("versiondisplay").innerHTML = "1.3";
 document.getElementById("changelog").innerHTML = 
 "<h3>ChangeLog</h3>"+
 "<ul>"+
 "<li>2016/06/19 01:45 Start."+
 "<li>2016/06/19 02:41 Implemented."+
 "<li>2016/06/19 20:00 The bug in which change size after load img occurs wrong size for saving was fixed."+
+"<li>2016/06/19 22:00 Enlarged UI."+
 "</ul>";
 document.getElementById("knownbugs").innerHTML = 
 "<h3>KnownBug</h3>"+
