b2f0546f245e6b3e2810910ced5565228dbd78ce
koteitan <ukaibutton@mac.com>
2025-05-07 05:10:27 +0900

    v1.4/Improved design

--- version.js
+++ version.js
@@ -1,11 +1,11 @@
-document.getElementById("versiondisplay").innerHTML = "1.3";
+document.getElementById("versiondisplay").innerHTML = "1.4";
 document.getElementById("changelog").innerHTML = 
 "<h3>ChangeLog</h3>"+
 "<ul>"+
-"<li>2016/06/19 01:45 Start."+
-"<li>2016/06/19 02:41 Implemented."+
-"<li>2016/06/19 20:00 The bug in which change size after load img occurs wrong size for saving was fixed."+
-"<li>2016/06/19 22:45 Enlarged UI."+
+"<li>2016/06/19 Implemented."+
+"<li>2016/06/19 The bug in which change size after load img occurs wrong size for saving was fixed."+
+"<li>2016/06/19 Enlarged UI."+
+"<li>2025/05/07 Improved design."+
 "</ul>";
 document.getElementById("knownbugs").innerHTML = 
 "<h3>KnownBug</h3>"+
